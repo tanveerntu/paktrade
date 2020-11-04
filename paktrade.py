c05189df410410dc15f1e8754e20153c4a06a037
Tanveer Hussain <73167258+tanveerntu@users.noreply.github.com>
2020-11-04 16:47:12 +0500

    Update paktrade.py

--- paktrade.py
+++ paktrade.py
@@ -2,9 +2,7 @@
 import streamlit as st
 import pandas as pd
 import requests
-Import plotly as py 
 import plotly.express as px
-import plotly.graph_objects as go
 
 api_url = 'https://comtrade.un.org/api/get?max=A&type=C&freq=A&px=HS&ps=now&r=586&p=all&rg=all&cc=TOTAL&fmt=csv'
 
